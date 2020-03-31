6087de3116d30f704025aeee4f87f1fb2aba8439
samarth <samarth.gugnani30@gmail.com>
2020-03-31 10:36:07 +0530

    added automatic phone number

--- app/src/main/java/com/example/dell/rare/UI/Login.java
+++ app/src/main/java/com/example/dell/rare/UI/Login.java
@@ -1,8 +1,12 @@
 package com.example.dell.rare.UI;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -21,6 +25,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.dell.rare.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialPickerConfig;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -31,7 +41,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.jar.Attributes;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int RESOLVE_HINT = 1011;
+    String mobNumber;
 
     TextInputLayout number_layout;
     TextInputLayout name_layout,password_layout;
@@ -62,7 +75,19 @@ public class Login extends AppCompatActivity {
             login = findViewById(R.id.login_button);
             password_layout = findViewById(R.id.text_password);
 
+            number_auto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getPhone();
+                }
+            });
 
+            number_layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getPhone();
+                }
+            });
 
             signup.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -89,6 +114,47 @@ public class Login extends AppCompatActivity {
             });
 
     }
+
+    private void getPhone() {
+       GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+               .addApi(Auth.CREDENTIALS_API)
+               .addConnectionCallbacks(this)
+               .addOnConnectionFailedListener(this)
+               .build();
+       googleApiClient.connect();
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setHintPickerConfig(new CredentialPickerConfig.Builder()
+                        .setShowCancelButton(true)
+                        .build())
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        PendingIntent intent =
+                Auth.CredentialsApi.getHintPickerIntent(googleApiClient, hintRequest);
+        try {
+            startIntentSenderForResult(intent.getIntentSender(), RESOLVE_HINT, null, 0, 0, 0,new Bundle());
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESOLVE_HINT) {
+            if (resultCode == RESULT_OK) {
+                Credential cred = data.getParcelableExtra(Credential.EXTRA_KEY);
+                if(cred != null){
+                    mobNumber = cred.getId();
+                    String newString = mobNumber.replace("+91","");
+                    number_auto.setText(newString);
+                }else{
+                    Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     private void Loginuser(final String number, final String password) {
             StringRequest stringRequest = new StringRequest(Request.Method.POST, loginapi, new Response.Listener<String>() {
                 @Override
@@ -238,5 +304,20 @@ public class Login extends AppCompatActivity {
         else{
             return true;
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- app/src/main/java/com/example/dell/rare/UI/signup.java
+++ app/src/main/java/com/example/dell/rare/UI/signup.java
@@ -1,8 +1,12 @@
 package com.example.dell.rare.UI;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
@@ -23,6 +27,12 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.balysv.materialripple.MaterialRippleLayout;
 import com.example.dell.rare.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialPickerConfig;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONException;
@@ -31,7 +41,10 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class signup extends AppCompatActivity {
+public class signup extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int RESOLVE_HINT = 1011;
+    String mobNumber;
 
     AutoCompleteTextView number_layout;
     AutoCompleteTextView name_layout,password_layout;
@@ -57,6 +70,13 @@ public class signup extends AppCompatActivity {
         signup = findViewById(R.id.signup_button);
         login = findViewById(R.id.login_button);
 
+        number_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPhone();
+            }
+        });
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,6 +101,47 @@ public class signup extends AppCompatActivity {
               }
             }
         });
+    }
+
+
+    private void getPhone() {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.CREDENTIALS_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        googleApiClient.connect();
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setHintPickerConfig(new CredentialPickerConfig.Builder()
+                        .setShowCancelButton(true)
+                        .build())
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        PendingIntent intent =
+                Auth.CredentialsApi.getHintPickerIntent(googleApiClient, hintRequest);
+        try {
+            startIntentSenderForResult(intent.getIntentSender(), RESOLVE_HINT, null, 0, 0, 0,new Bundle());
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESOLVE_HINT) {
+            if (resultCode == RESULT_OK) {
+                Credential cred = data.getParcelableExtra(Credential.EXTRA_KEY);
+                if(cred != null){
+                    mobNumber = cred.getId();
+                    String newString = mobNumber.replace("+91","");
+                    number_layout.setText(newString);
+                }else{
+                    Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
     private void createAccount(final String name,final String number, final String password) {
@@ -192,5 +253,20 @@ public class signup extends AppCompatActivity {
             password_layout.setError(null);
             return true;
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
